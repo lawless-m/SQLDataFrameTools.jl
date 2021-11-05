6d2ebaefe4f7d656e691ab324055a2ecd4ede9a6
Matthew Heath <Matthew.Heath@wrenkitchens.com>
2021-11-05 10:49:20 +0000

    added basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using SQLDataFrameTools
 using Test
 
+
+
 @testset "SQLDataFrameTools.jl" begin
-    # Write your tests here.
+   @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.now())
+   @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.Day(1))
+   @test SQLDataFrameTools.cachepath("a", "hash", :jdf) == "a\\hash.jdf"
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, dictencode=false), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, subformat=:zip), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, dictencode=false, subformat=:zip), SQLDataFrameTools.QueryCache)
 end

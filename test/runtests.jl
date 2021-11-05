b8edd54aae4c7566b8d6b72cb82757437ed31fbe
Matthew Heath <Matthew.Heath@wrenkitchens.com>
2021-11-05 11:05:45 +0000

    test only worked on Windows but not point using joinpath as that's just what the code does!

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
 @testset "SQLDataFrameTools.jl" begin
    @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.now())
    @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.Day(1))
-   @test SQLDataFrameTools.cachepath("a", "hash", :jdf) == "a\\hash.jdf"
+   @test endswith(SQLDataFrameTools.cachepath("a", "hash", :jdf), "hash.jdf")
    @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf), SQLDataFrameTools.QueryCache)
    @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, dictencode=false), SQLDataFrameTools.QueryCache)
    @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, subformat=:zip), SQLDataFrameTools.QueryCache)

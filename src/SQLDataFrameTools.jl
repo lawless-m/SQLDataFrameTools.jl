d8351f77cf1f8fcd9091a900001ecedd43624077
Matthew Heath <Matthew.Heath@wrenkitchens.com>
2021-11-05 12:13:25 +0000

    added docs, changed params of expired

--- src/SQLDataFrameTools.jl
+++ src/SQLDataFrameTools.jl
@@ -6,8 +6,22 @@ using DBInterface
 using Dates
 using DataFrames
 
-export QueryCache, df_cached, select_fn
+export QueryCache, df_cached, select_fn, expired
 
+"""
+QueryCache(sql, select, dir, format;  subformat=nothing, dictencode=true)
+is the Data Structure used in this module.
+
+`sql` is the SQL Query
+`select` is the function used to retrieve the dataset (which can be created with select_sql)
+`dir` is the directory in which to store the cache file
+`format` is one of the file formats supported. See DataFrameTools.df_formats() for the list.
+`subformat` and `dictencode` are optional parameters for DataFrameTools.df_write
+
+Example:
+
+query = QueryCache("SELECT 1",  select_fn(MySQL.Connection, SERVER, USER, PASSWORD), ".", :jdf)
+"""
 struct QueryCache
 	sql
 	select
@@ -19,9 +33,38 @@ end
 
 cachepath(dir, hash, sformat) = joinpath(dir, hash * '.' * string(sformat))
 
-expired(path, expires::DateTime) = expires <= now() || stat(path).device == 0
-expired(path, ttl::Period) = expired(path, unix2datetime(mtime(path)) + ttl)
+"""
+expired(q::QueryCache, expires::DateTime)
+expired(q::QueryCache, ttl::Period)
+
+Has the cache file expired. If the file doesn't exist always return true, otherwise check the mtime against the given time
+
+`expired(query, Dates.Day(3))` # returns a bool of whether the cache is more than 3 days old 
+`expired(query, Dates.Minute(30))` # returns a bool of whether the cache is more than 30 minutes old 
+`expired(query, Dates.DateTime(2021, 1, 1, 1, 2, 3))` # returns a bool compared to the given time
+
+"""
+expired(q::QueryCache, expires::DateTime) = expires <= now() || stat(q.cachepath).device == 0
+expired(q::QueryCache, ttl::Period) = expired(q.cachepath, unix2datetime(mtime(q.cachepath)) + ttl)
 	
+"""	
+df_cached[q, ttl_e; noisy=false)
+
+ttl_e is either a DataTime or Period specifying how long the cache should be live.
+
+If the cached version of the DataFrame is still live, return it from disk.
+
+If it has expired, call the Query's sql_function with the sql, write the DataFrame to disk 
+in the specified formats and return the DataFrame.
+
+The noisy flag indicates whether to print where the data is coming from to stderr.
+
+Example
+
+`df = df_cached(query, now())` # this will always retrive the SQL from the server
+`df = df_cached(query, Dates.Day(7), noisy=true)` # this use the cached version until it is 7 days old and will print "From Server" or "From Cache" depending where it came from.
+
+"""
 function df_cached(q::QueryCache, ttl_e; noisy=false)
 	if expired(q.cachepath, ttl_e)
 		if noisy
@@ -38,9 +81,17 @@ function df_cached(q::QueryCache, ttl_e; noisy=false)
 	end
 end
 
+"""
+select_fn(conn, server, user, password) = sql->...
+
+Return a function which will `execute` the supplied sql
+
+Example:
+
+sql_fn = select_fn(MySQL.Connection, SERVER, USER, PASSWORD)
+
+"""
 select_fn(conn, server, user, password) = sql->DBInterface.execute(DBInterface.connect(conn, server, user, password), sql) |> DataFrame 
-# e.g.
-# sql_fn = select_fn(MySQL.Connection, SERVER, USER, PASSWORD)
 
 ###
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,12 @@
 using SQLDataFrameTools
 using Test
 
-
-
 @testset "SQLDataFrameTools.jl" begin
-   @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.now())
-   @test SQLDataFrameTools.expired("", SQLDataFrameTools.Dates.Day(1))
+   @test SQLDataFrameTools.expired(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf), SQLDataFrameTools.Dates.now())
+   @test SQLDataFrameTools.expired(SQLDataFrameTools.QueryCache("SELECT 2", ()->true, ".", :jdf), SQLDataFrameTools.Dates.Day(1))
    @test endswith(SQLDataFrameTools.cachepath("a", "hash", :jdf), "hash.jdf")
-   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf), SQLDataFrameTools.QueryCache)
-   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, dictencode=false), SQLDataFrameTools.QueryCache)
-   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, subformat=:zip), SQLDataFrameTools.QueryCache)
-   @test isa(SQLDataFrameTools.QueryCache("SELECT 1", ()->true, ".", :jdf, dictencode=false, subformat=:zip), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 3", ()->true, ".", :jdf), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 4", ()->true, ".", :jdf, dictencode=false), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 5", ()->true, ".", :jdf, subformat=:zip), SQLDataFrameTools.QueryCache)
+   @test isa(SQLDataFrameTools.QueryCache("SELECT 6", ()->true, ".", :jdf, dictencode=false, subformat=:zip), SQLDataFrameTools.QueryCache)
 end
